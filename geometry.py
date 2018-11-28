258403ae7f28e7d85c18b9312c0ae3a9e9661cdc
Cloclo29 <clotilde.antona29@gmail.com>
2018-11-28 09:39:32 +0100

    rajout de z dans le module geom

--- geometry.py
+++ geometry.py
@@ -2,18 +2,19 @@
 import math
 
 class Point:
-    def __init__(self,x,y):
+    def __init__(self,x,y,z):
         self.x = x  #coordonnée x en mètres 
         self.y = y  #coordonée y en mètres
+        self.z = z
         
     def __repr__(self):
-        return str(self.x) + ',' + str(self.y)
+        return '(' + str(self.x) + ',' + str(self.y) + ',' + str(self.z) + ')'  # affichage du point à changer si jamais inutile
         
     def __sub__(self, other):
-        return Point(self.x - other.x , self.y - other.y) #soustraction 
+        return Point(self.x - other.x , self.y - other.y , self.z - other.z) #soustraction 
         
     def __add__(self , other):
-        return Point(self.x + other.x , self.y + other.y)
+        return Point(self.x + other.x , self.y + other.y , self.z - other.z)
         
     #def __rmul__(self, k):
         #return Point(k * self.x, k * self.y)
@@ -26,7 +27,8 @@ class Line: #ligne reliant 2 points
     def lenght(self):
         a = self.Coords[0][0] - self.Coords[1][0]
         b = self.Coords[0][1] - self.Coords[1][1]
-        return math.sqrt(a**2 + b**2) #calcul de la longueur de la ligne entre A et B
+        c = self.Coords[0][2] - self.Coords[1][2]
+        return math.sqrt(a**2 + b**2 + c**2) #calcul de la longueur de la ligne entre A et B
         
     
         
