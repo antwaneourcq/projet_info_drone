48e76d61ae2b5a0ffd97f93a27296b8195a9cbaf
antwaneourcq <antoine.leclercq27@gmail.com>
2018-11-28 09:43:59 +0100

    add trajet module

---
+++ trajet.py
@@ -0,0 +1,14 @@
+ALTI_CROIS = 2000
+
+
+def attribuer_mission(entrepots, clients, flottes_drones):
+	#-->[(EiCj, drones, ordre de priorité?)]
+        pass
+
+def decoupe_trajet([(EiCj, drones, ordre de priorité?)]):
+	#--> '''segment en 4D (7 points)'''
+        pass
+
+def liste_mission(mission):
+	#cumulation de attribuer mission et decoupe trajet
+        pass
