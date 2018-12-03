0f8c2bf22e2e9ad417130f265ee206e063344e88
Camille Lescuyer <camillelescuyer@MacBook-Air-de-Camille-2.local>
2018-12-03 13:47:53 +0100

    fusion de fichier

--- map.py
+++ map.py
@@ -1,3 +1,14 @@
 
 def basic_map():
     '''meter'''
+
+
+#carte est une liste de 2 tuples, donnant le coin supérieur gauche et le coin inférieur droit
+
+def carre_int(carte):
+#je creer l'environnement,
+# environnement = liste de deux intervalles représenté par des tuples et correspond à l'intervalle des abcsisses et des ordonnées
+    p = 5/100
+    a,b,c,d = carte[0][0], carte[0][1] , carte[1][0] , carte[1][1]
+    #je defint les limites de l'espace intérieur pour les clients
+    return = a+p*l_x , c-p*l_x , d+p*l_y , b-p*l_y
