6cfc416b7413dde6d5af378818ee0e063e4ea023
Camille Lescuyer <camillelescuyer@MacBook-Air-de-Camille-2.local>
2018-12-03 14:05:16 +0100

    correction erreur

--- map.py
+++ map.py
@@ -15,7 +15,7 @@ def carre_int(carte):
     p = 5/100
     a,b,c,d = carte[0][0], carte[0][1] , carte[1][0] , carte[1][1]
     #je defint les limites de l'espace intérieur pour les clients
-    return = a+p*l_x , c-p*l_x , d+p*l_y , b-p*l_y
+    return a+p*l_x , c-p*l_x , d+p*l_y , b-p*l_y
 
 
 
