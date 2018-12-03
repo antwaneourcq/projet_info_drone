a3578977f3774648ba33572edb54088453819e7e
Camille Lescuyer <camillelescuyer@MacBook-Air-de-Camille-2.local>
2018-12-03 14:02:49 +0100

    hcangement des commentaires

--- tirage_au_sort.py
+++ tirage_au_sort.py
@@ -15,12 +15,16 @@ NMAX_EN = 50 #nombre maximal d'entrepôts autorisés
 
 
 def points_utiles(carte):
-    #renvoie la liste des entrepots et des clients
+    '''renvoie la liste des entrepots et des clients
+    uniform genere un nombre réel aléatoire dans l'intervalle donné
+    50 correspond au nombre maximum de clients ou d'entrepots , choisi arbitrairement
+    uniform génère un nombre réel aléatoire dans l'intervalle donné
 
-    #uniform genere un nombre réel aléatoire dans l'intervalle donné
-    # 50 correspond au nombre maximum de clients ou d'entrepots , choisi arbitrairement
+    je decoupe le contour en quatre espace , 1=espace superieur , 2= espace droit , 3= espace inferieur , 4= espace gauche
 
-    #uniform génère un nombre réel aléatoire dans l'intervalle donné
+    carre_ext renvoie la list de l'intervalle des abcisse et de l'intervalle des ordonnées pour chaque espace 1,2,3,4
+
+    p est assimilé à l'un de ces espaces alétoirement'''
     nbr_entrepots = uniform((5, NMAX_EN))
     nbr_clients = uniform((0, NMAX_CL))
 
@@ -28,28 +32,25 @@ def points_utiles(carte):
     l_clients =[]
     x0,x1,y0,y1 = map.carre_int(carte)
     a, b, c, d = carte[0][0], carte[0][1], carte[1][0], carte[1][1]
-    # je decoupe le contour en quatre espace , 1=espace superieur , 2= espace droit , 3= espace inferieur , 4= espace gauche
-    # carre_ext renvoie la list de l'intervalle des abcisse et de l'intervalle des ordonnées pour chaque espace 1,2,3,4
+
     carre_ext = [[(x0, c), (y1, b)], [(x1, c), (d, y1)], [(a, x1), (c, y0)], [(a, x0), (y0, b)]]
     for _ in range(nbr_entrepots) :
-        #p est assimilé à l'un de ces espaces aléatoireent
-        p= randint(1,4)
-        Point.x = uniform(carre_ext[p][0])
-        Point.y = uniform(carre_est[p][1])
-        Point.z = 0
+
+        p = randint(1, 4)
+        x,y,z = uniform(carre_ext[p][0]),uniform(carre_ext[p][1]),0
+        Point = geometry.Point()
         l_entrepots.append(Point)
 
     for _ in range(nbr_clients):
-        Point.x = uniform(x0,x1)
-        Point.y = uniform(y0,y1)
-        Point.z = 0
+        x,y,z = uniform(x0,x1) , uniform(y0,y1) , 0
+        Point
         l_client.append(Point)
 
     return l_entrepots,l_clients
 
 
 def drones_utiles(dictionnary0,carte):
-    #renvoie un dictionnaire assimilaant entre 1 et 10 drones à un entrepot
+    '''renvoie un dictionnaire assimilaant entre 1 et 10 drones à un entrepot'''
     l_entrepots = points_utiles(carte)[0]
 
     for entrepots in points_utiles(carte)[0]:
