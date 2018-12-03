1a7b959dae9e744f8bd120d352e69816fc47799e
antwaneourcq <antoine.leclercq27@gmail.com>
2018-12-03 10:41:38 +0100

    relecture de Tirage au sort ajout de constantes globales utiles

--- tirage_au_sort.py
+++ tirage_au_sort.py
@@ -1,15 +1,16 @@
 import geometry
-
 from random import uniform, randint
 
+NMAX_CL = 50 #nombre maximal de clients autorisés
+NMAX_EN = 50 #nombre maximal d'entrepôts autorisés
 #carte est une liste de 2 tuples, donnant le coin supérieur gauche et le coin inférieur droit
 
 def carre_int(carte):
-#je creer l'environnement,
-# environnement = liste de deux intervalles représenté par des tuples et correspond à l'intervalle des abcsisses et des ordonnées
+#je crée l'environnement,
+# environnement = liste de deux intervalles représentés par des tuples, correspondant à l'intervalle des abcsisses et des ordonnées
     p = 5/100
     a,b,c,d = carte[0][0], carte[0][1] , carte[1][0] , carte[1][1]
-    #je defint les limites de l'espace intérieur pour les clients
+    #je définis les limites de l'espace intérieur pour les clients
     return = a + p * l_x , c - p  * l_x , d + p*l_y , b - p*l_y
 
 
@@ -17,10 +18,9 @@ def carre_int(carte):
 
 def points_utiles(carte):
     #renvoie la liste des entrepots et des clients
-    #uniform genere un nombre réel aléatoire dans l'intervalle donné
-    # 50 correspond au nombre maximum de clients ou d'entrepots , choisi arbitrairement
-    nbr_entrepots = uniform((5,50))
-    nbr_clients = uniform((0,50))
+    #uniform génère un nombre réel aléatoire dans l'intervalle donné
+    nbr_entrepots = uniform((5, NMAX_EN))
+    nbr_clients = uniform((0, NMAX_CL))
     l_entrepots = []
     l_clients =[]
     x0,x1,y0,y1 = carre_int(carte)
