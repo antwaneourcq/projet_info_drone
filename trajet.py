4bf8f156e5629a6c7957ee12eac2c20ab566146e
Fanny Haff <fannyhaff@MBP-de-Fanny.connectify>
2018-12-03 14:59:56 +0100

    réctification module trajet selon modificications des modules tirage au sort et drones

--- trajet.py
+++ trajet.py
@@ -1,32 +1,33 @@
 import tirage_au_sort
 import drones as d
+import geometry
 
 ALTI_CROIS = 200 #en mètres
 
 
 def ordre_priorite_drones(dico, drones): 
 	#prend en argument une liste de drones à trier selon leur vitesse maximale
-	drone.sort(key=d.get_h_speeds(dico, drone)[0] reverse=True) #on trie les drones de l'entrepot le plus proche par ordre décroissant de vitesse maximale en route (tri en place)
+	drone.sort(key=d.get_h_speeds(dico, drone)[0], reverse=True) #on trie les drones de l'entrepot le plus proche par ordre décroissant de vitesse maximale en route (tri en place)
 	return drone
 
 
-def attribuer_mission(carte):
+def attribuer_mission(dico, carte):
 	#renvoie une liste de listes [[x,y] coordonnées du client, [x,y] coordonnées de l'entrepot le plus proche du client, drone attribué à la mission, liste d'autres drones par ordre de priorité si drone attribué non dispo]
 	entrepots,clients=tirage_au_sort.points_utiles(carte)
-	drones=tirage_au_sort.drones_utiles()  #j'ai supposé que drones_utiles renvoie une liste de listes de listes de drones correspondants aux entrepots de la liste l_entrepots de la fonction points_utiles
+	drones=tirage_au_sort.drones_utiles(dico,carte)  #j'ai supposé que drones_utiles renvoie une liste de listes de listes de drones correspondants aux entrepots de la liste l_entrepots de la fonction points_utiles
 	nb_entrepots=len(entrepots)
 	l=[]
 	for cli in clients: #j'ai supposé que clients était comme entrepots, une liste de coordonnées (x,y) des clients tirés au sort dans tirage_au_sort
-		distance=sqrt((cli[0]-entrepots[0][0])**2+(cli[1]-entrepots[0][1])**2)
+		distance=sqrt((cli.x-entrepots[0].x)**2+(cli.y-entrepots[0].y)**2)
 		e=entrepots[0]
 		ind=0
 		for i in range(nb_entrepots):
-			if sqrt((cli[0]-entrepots[i][0])**2+(cli[1]-entrepots[i][1])**2)<distance: #calcul de l'entrepot le plus proche du client cli
+			if sqrt((cli.x-entrepots[i].x)**2+(cli.y-entrepots[i].y)**2)<distance: #calcul de l'entrepot le plus proche du client cli
 				distance=sqrt((cli[0]-ent[0])**2+(cli[1]-ent[1])**2)
 				e=entrepots[i] 
 				ind=i
-		d=ordre_priorite_drones(drones[ind]) 
-		l.append([cli[0],cli[1]],[e[0],e[1]],d[0],d)
+		drone_ord=ordre_priorite_drones(drones[ind][1]) 
+		l.append([cli.x,cli.y],[e.x,e.y],d[0],drone_ord)
 	return l 
 	
 def calcul_duree_mission(drone,p1,p4): 
@@ -34,7 +35,7 @@ def calcul_duree_mission(drone,p1,p4):
 	dico=drones.drones_list(drones.read("aircraft.json"))
 	vit_vert=d.get_v_speeds(dico, drone)[0]
 	vit_hori=d.get_h_speeds(dico, drone)[0]
-	distance=sqrt((p1[0]-p4[0])**2+(p1[1]-p4[1])**2)
+	distance=sqrt((p1.x-p4.x)**2+(p1.y-p4.y)**2)
 	return 2*(ALTI_CROIS/vit_vert)+2*(distance/vit_hori)
 
 
@@ -44,10 +45,10 @@ def decoupe_trajet(carte):
 	nb_missions=len(l)
 	s=[0]*nb_missions
 	for i in range(nb_missions):
-		p1=l[i][1]+[0]  #+[0] correspon à la cooronnée en altitude que je rajoute aux coordonnées de point p1
-		p2=l[i][1]+[ALTI_CROIS]
-		p3=l[i][0]+[ALTI_CROIS]
-		p4=l[i][0]+[0]
+		p1=geometry.Point(l[i][1][0],l[i][1][1],0)  #0 correspond à la coordonnée en altitude que je rajoute aux coordonnées de point p1
+		p2=geometry.Point(l[i][1][0],l[i][1][1],ALTI_CROIS)
+		p3=geometry.Point(l[i][0][0],l[i][1][1],ALTI_CROIS)
+		p4=geometry.Point(l[i][0][0],l[i][1][1],0)
 		p5=p3
 		p6=p2
 		p7=p1
@@ -58,5 +59,7 @@ def decoupe_trajet(carte):
 
 def liste_mission(mission):
 	#cumulation de attribuer mission et decoupe trajet
+	pass
+	
 	
 
