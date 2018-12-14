633e1fdbfb2b1c2504f0c3f8db6bd87153cff541
Cloclo29 <clotilde.antona29@gmail.com>
2018-12-14 10:50:59 +0100

    modifiction mappy: fctions carte_ int et verif_map

--- mappy.py
+++ mappy.py
@@ -15,27 +15,20 @@ Z_ALT = 1500
 
 
 def verif_map(P):
-    #A_origin = geo.Point(0, 0, Z_ALT)    donner inutile ou alors l'utiliser pour la comparaison avec les zeros attende confirmation de suppression
+    #pas d'influence du A puisuq ec'est le point nul
     C_map = conversion_deg_m(C)
-    return P.x <= C_map.x and P.y <= C_map.y and P.x >= 0 and P.y >= 0
+    return P.x <= C_map.x and P.y <= C_map.y  and P.x >= 0 and P.y >= 0
 
-#carte est une liste de 2 tuples, donnant le coin supérieur gauche et le coin inférieur droit
 
-def carre_int(carte):
+
+def carre_int(A_map,C_map):
 #je crée l'environnement,
 #environnement = liste de deux intervalles représenté par des tuples et correspond à l'intervalle des abcsisses et des ordonnées
     p = 5/100
-    '''C_map = conversion_deg_m(C)
-    A_origin = geo.Point(0,0,Z_ALT)
-    l_x, l_y = C.x-A.x, C.y-A.y
-    #je definis les limites de l'espace intérieur pour les clients
-    return geo.Point(A.x+p*l_x , A.y+p*l_y ,Z_ALT) , geo.Point(C_map.x-p*l_x , C_map.y-p*l_y , Z_ALT)'''
-    A_map = geo.Point(0, 0, Z_ALT)
-    print(carte)
-    C_map = carte[1] #geo.Point(C.long, C.lat, C.z)
-    #print('hi sir', conversion_deg_m(C))
-    dx, dy = p * (C_map.x - A_map.x), p * (C_map.y - A_map.y)
-    return geo.Point(A_map.x + dx, A_map.y + dy, Z_ALT), geo.Point(C_map.x - dx, C_map.y - dy, Z_ALT)
+    '''je definis les limites de l'espace intérieur pour les clients'''
+    dx, dy = p * (C_map.x), p * (C_map.y)
+    return geo.Point(dx, dy, Z_ALT), geo.Point(C_map.x - dx, C_map.y - dy, Z_ALT)
+
 
 def conversion_m_deg(P):  #convertir des coordonées x,y en DD
     '''on passe de x en metres à x en degrés'''
