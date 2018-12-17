3732145e423048185e3aa724b1a32320ac226901
antwaneourcq <antoine.leclercq27@gmail.com>
2018-12-17 08:13:04 +0100

    test de trajet, test basé sur le main

--- main.py
+++ main.py
@@ -13,17 +13,45 @@ def main():
     dico = lect_dr.read(FILE)
     models = lect_dr.listmodels(dico)
     carte = (mappy.A, mappy.C)
-    l_entrepots, l_clients, carre_ext = tas.points_utiles(carte)
+    entrepots, clients, carre_ext = tas.points_utiles(carte)
     x_entrepots,y_entrepots , x_clients, y_clients =[],[] , [],[]
-    for i in range(len(l_entrepots)):
-        x_entrepots.append(l_entrepots[i].x)
-        y_entrepots.append(l_entrepots[i].y)
-    for i in range(len(l_clients)):
-        x_clients.append(l_clients[i].x)
-        y_clients.append(l_clients[i].y)
+    for i in range(len(entrepots)):
+        x_entrepots.append(entrepots[i].x)
+        y_entrepots.append(entrepots[i].y)
+    for i in range(len(clients)):
+        x_clients.append(clients[i].x)
+        y_clients.append(clients[i].y)
     plt.plot(x_entrepots,y_entrepots, '.')
     plt.plot(x_clients,y_clients, '.')
     plt.show()
-    print(tas.drones_utiles(dico, l_entrepots))
-
+    print(entrepots)
+    print('avant')
+    tas.drones_utiles(dico, entrepots)
+    print(entrepots)
+    model_prio = trajet.ordre_priorite_drones(models)
+    #(model_prio)
+    #for model in model_prio:
+    #    print(model, model.v_speed_max)
+    '''
+    entrepot0 = tas.Entrepot(0,0,0, models)
+    print(entrepot0)
+    entrepot0.addDrone(lect_dr.Drone('Amzn', geo.Point(entrepot0.x, entrepot0.y, entrepot0.z)))
+    print(entrepot0, '\n Les drones', entrepot0.drones, '\n Amazon Drone', entrepot0.models['Amzn'])
+    for drone in entrepot0.drones:
+        print(drone, entrepot0.drones, type(drone))
+    print('capacité drone : ',capacite_drone(entrepot0, geo.Point(20,20,20)))
+    '''
+    print('\n\nya', entrepots[0], entrepots[0].models)
+    missions = trajet.attribuer_mission(entrepots, clients)
+    print('missions : ')
+    mission = missions[0]
+    client = mission[0]
+    entrepot = mission[1]
+    print(client, '||||', entrepot)
+    p1, p2, p3, p4, dt = trajet.decoupe_trajet(mission)
+    print(p1,p2,p3,p4,dt)
+    print(mappy.A, mappy.B, mappy.C, mappy.D)
+    dep = mappy.conversion_m_deg(p2)
+    arr = mappy.conversion_m_deg(p3)
+    print(dep, arr)
 main()
--- tirage_au_sort.py
+++ tirage_au_sort.py
@@ -5,18 +5,36 @@ import matplotlib.pyplot as plt
 
 NMAX_CL = 50 #nombre maximal de clients autorisés
 NMAX_EN = 10 #nombre maximal d'entrepôts autorisés
+NMAX_DR = 10
 #carte est une liste de 2 tuples, donnant le coin supérieur gauche et le coin inférieur droit
+
+dico = lect_dr.read("aircraft.json")
+MODELS = lect_dr.listmodels(dico)
+    
 class Entrepot(geo.Point):
     
     def __init__(self, x, y, z, models):
         super().__init__(x, y, z)
-        self.drones = {}
+        self.models = {}
         for model in models:
-            self.drones[str(model)] = 0
+            self.models[str(model)] = 0
+        self.drones = []
     
     def __repr__(self):
-        return '(' + str(self.x) + ',' + str(self.y) + ',' + str(self.z) +')\n' + 'drones : ' +str(self.drones)
+        return '(' + str(self.x) + ',' + str(self.y) + ',' + str(self.z) +')' + ' drones : ' +str(self.drones)
     
+    def addDrone(self, drone):
+        model= str(drone.model)
+        self.models[model] += 1
+        if self.models[model] == 1:
+            self.drones.append(model)
+    
+    #def maj_drone_effectifs(self):
+    #    '''met à jour le dictionnaire de "drones_effectifs" par rapport aux données du dictionnaire "drones"'''
+    #    for model in self.models:
+    #        pass
+        
+        
 def points_utiles(carte):
     '''renvoie la liste des entrepôts et des clients
     uniform génère un nombre réel aléatoire dans l'intervalle donné
@@ -31,7 +49,7 @@ def points_utiles(carte):
     for _ in range(nbr_entrepots) :
         p = randint(0, 3)
         x,y = random.uniform(carre_ext[p][0][0],carre_ext[p][0][1]),random.uniform(carre_ext[p][1][0],carre_ext[p][1][1])
-        l_entrepots.append(Entrepot(x, y, 0, models))
+        l_entrepots.append(Entrepot(x, y, 0, MODELS))
     for _ in range(nbr_clients):
         l_clients.append(geo.Point(random.uniform(A_int.x,C_int.x) ,random.uniform(A_int.y,C_int.y) , 0))
     return l_entrepots,l_clients , carre_ext
@@ -40,18 +58,17 @@ def drones_utiles(dico, entrepots):
     '''renvoie un dictionnaire assimilant entre 1 et 10 drones à un entrepot'''
     models = lect_dr.listmodels(dico)
 
-    for entrepot in l_entrepots:
-        p=randint(1,10)
-        p = 50
+    for entrepot in entrepots:
+        p=randint(1,NMAX_DR)
         for _ in range (p) :
             drone = random.choice(models)
-            entrepot.drones[str(drone)] += 1
-    return entrepots
+            entrepot.addDrone(drone)
+    #return entrepots
 
 
 def test():
     dico = lect_dr.read("aircraft.json")
-    model = lect_dr.listmodels(dico)
+    models = lect_dr.listmodels(dico)
     carte = (mappy.A, mappy.C)
     l_entrepots, l_clients, carre_ext = points_utiles(carte)
     x_entrepots,y_entrepots , x_clients, y_clients =[],[] , [],[]
@@ -65,7 +82,7 @@ def test():
     plt.plot(x_clients,y_clients, '.')
     plt.show()
     print(drones_utiles(dico, l_entrepots))
-
+#test()
 
 
 
